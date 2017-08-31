247fe9f6e7689f85ec77824950483a0e6b0186c5
elbean <elbean@users.noreply.github.com>
2017-08-31 10:05:35 -0600

    Simple copy task
    Allows for copying files and entire directories

---
+++ Copy/Copy.ps1
@@ -0,0 +1,23 @@
+﻿#parameters
+param(
+    [string]$source = "",
+    [string]$destination = ""
+)
+
+$validationErrors = [string]::Empty
+
+#validate parameters
+if([string]::IsNullOrWhiteSpace($source)){ $validationErrors += "Parameter -source is required `r`n" }
+if([string]::IsNullOrWhiteSpace($destination)){ $validationErrors += "Parameter -destination is required`r`n" }
+if(![string]::IsNullOrWhiteSpace($validationErrors))
+{ 
+	Write-Error "$validationErrors"
+    exit 1
+}
+
+#create the directory
+Write-Host "Creating destination directory $destination"
+New-Item -ItemType Directory -Force -Path $destination
+
+Write-Host "Copying from $source to $destination"
+Copy-Item $source $destination -Recurse
